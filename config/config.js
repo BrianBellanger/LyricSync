43c6628aa24c4b2bc5e5c6fea067857d112fe3af
Brian Bellanger <brianjbellanger@gmail.com>
2026-05-15 13:35:41 -0500

    Changed Google API to restricted key - removed config.js from gitignore

---
+++ config/config.js
@@ -0,0 +1,10 @@
+// This API key is restricted to the domain ".lyricsync-app.vercel.app" and the Google YouTube API. 
+// It is not intended for public use and should not be shared or used in any other context.
+// If you need to use the Google API for your own project, please obtain your own API key from the Google Cloud Console and 
+// ensure that it is properly secured and restricted to prevent unauthorized access.
+
+let config = {
+  GOOGLE_API_KEY: "AIzaSyAZXABx0LwpVSPbAphTP3gE5lZHYraupMo"
+};
+
+export default config;
